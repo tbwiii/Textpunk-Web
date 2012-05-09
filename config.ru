ba9d8fea101940ae81a01b73b5a9fea624f0f441
Ted Waller <hello@trippwaller.com>
2012-05-08 23:58:23 -0400

    initial commit

---
+++ config.ru
@@ -0,0 +1,2 @@
+require 'textpunk'
+run Sinatra::Application
---
+++ textpunk.rb
@@ -0,0 +1,38 @@
+require "sinatra"
+require "erb"
+require "sass"
+
+module Textpunkweb
+
+	get '/' do
+   		redirect("/game")
+	end
+
+	get '/game' do
+		erb :show_room, :locals => {:room => CURRENT}			
+
+	end
+
+	post '/game' do
+
+		action = params[:action]
+
+		CURRENT.paths.each_key do |key|
+			if action.include? key
+				CURRENT = CURRENT.go(action)
+			end
+		end
+
+		redirect("/game")
+		
+	end
+
+	get '/screen.css' do
+  		scss :screen
+  	end
+
+  	get '/favicon.ico' do
+  		:favicon
+  	end
+
+end
